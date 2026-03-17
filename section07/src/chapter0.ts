145952772064c289415fe304619e3d4b734675da
SangJun Park <wadqse1004@naver.com>
2026-03-17 23:43:27 +0900

    제네릭 1일차 정리

---
+++ section07/src/chapter0.ts
@@ -0,0 +1,17 @@
+/**
+ * 제네릭
+ */
+
+//제네릭 함수
+function func<T>(value: T): T{
+    return value;
+}
+
+let num = func(10);
+num.toFixed();
+
+let bool = func(true);
+let str = func("str");
+str.toUpperCase();
+
+let arr = func<[number,number,number]>([1,2,3]);
---
+++ section07/src/chapter1.ts
@@ -0,0 +1,38 @@
+/**
+ *  타입 변수
+ *  첫번쨰 사례
+ */
+
+function swap<T, U>(a: T, b: U){
+    return [b, a]
+}
+
+const [a, b] = swap("1",2);
+
+/**
+ *  두번째 사례
+ */
+
+function returnFirstValue<T>(data: [T, ...unknown[]]){
+    return data[0];
+}
+
+let num = returnFirstValue([0,1,2]);
+
+let str = returnFirstValue([1, "one", "two"]);
+
+/**
+ * 세번째 사례
+ */
+
+function getLength<T extends {length: number}>(data: T){
+    return data.length;
+}
+
+let var1 = getLength([1,2,3]);
+
+let var2 = getLength("12345");
+
+let var3 = getLength({length: 10});
+
+//let var4 = getLength(10);
